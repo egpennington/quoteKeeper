73c5bc7a54e93f080b927047b8fe83569341c54e
egpennington <egpennington@hotmail.com>
2025-07-16 12:35:26 -0800

    add p to motto

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -5,7 +5,9 @@ function SplashPage({ onEnter }) {
     <div className="splash-container">
       <h1 className="title">📚 quoteKeeper</h1>
       <p className="motto">
-        “One quote can change a moment. A library of them can change a life.”
+        “One quote can change a moment.
+      </p>
+      <p>"A library of them can change a life.”
       </p>
       <button className="enter-btn" onClick={onEnter}>
         Enter the Library
