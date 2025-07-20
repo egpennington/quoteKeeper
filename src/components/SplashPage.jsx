c18fd0dceab5cab9c9ea37242273ed4bf9f6e44a
egpennington <egpennington@hotmail.com>
2025-07-20 15:53:26 -0800

    delete extra quotes

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -5,9 +5,9 @@ function SplashPage({ onEnter }) {
     <div className="splash-container">
       <h1 className="title">📚 quoteKeeper</h1>
       <p className="motto">
-        One quote can change a moment.
+        "One quote can change a moment.
       </p>
-      <p className="moto">A library of them can change a life.""
+      <p className="moto">A library of them can change a life."
       </p>
       <button className="enter-btn" onClick={onEnter}>
         Enter the Library
